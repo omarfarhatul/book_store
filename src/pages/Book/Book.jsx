6b851091abd871c851bb2b3025aaffbb8fe58076
omarfarhatul <omarfarhatulhossain@gmail.com>
2026-03-04 23:21:42 +0600

    update date

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,25 +1,27 @@
 import React, { use } from 'react';
 
-const Book = ({singleBook}) => {
+const Book = ({ singleBook }) => {
     // const data=use(bookPromise);
     // console.log(data);
-    console.log(singleBook);
+
+    const { bookName, author, image }=singleBook;
+
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
-  <figure className="px-10 pt-10">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes"
-      className="rounded-xl" />
-  </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+        <div className="card bg-base-300 w-96 md:w-90 shadow-sm mx-auto">
+            <figure className="p-5">
+                <img
+                    src={image}
+                    alt="Shoes"
+                    className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">Card Title</h2>
+                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
     );
 };
 
