81943297cad8637d8ac91aab9b258bc4975a9ba3
omarfarhatul <omarfarhatulhossain@gmail.com>
2026-03-11 21:03:58 +0600

    add button

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import { addToStoredDB } from '../../utility/addToDB';
 
 const BookDetails = () => {
-    const {id}=useParams();
-    const bookId=parseInt(id);
-    const data=useLoaderData();
-    const singleBook=data.find(book=>book.bookId===bookId);
-    const {bookName, image}=singleBook;
+    const { id } = useParams();
+    const bookId = parseInt(id);
+    const data = useLoaderData();
+    const singleBook = data.find(book => book.bookId === bookId);
+    const { bookName, image } = singleBook;
+
+    const handleMarkAsRead=id=>{
+        //  Store with Id
+        // where to store 
+
+        addToStoredDB(id);
+    }
 
     return (
-        <div className='border-2 w-1/3 mx-auto text-center py-5'>
+        <div className='border-2 w-2/5 mx-auto text-center py-5'>
             <img className='w-40 mx-auto' src={image} alt="" />
             <h5>{bookName}</h5>
+
+            <button onClick={()=>handleMarkAsRead(id)} className="btn btn-active btn-accent mx-1">Mark as Read</button>
+            <button className="btn btn-active btn-info">Add to WishList</button>
         </div>
     );
 };
