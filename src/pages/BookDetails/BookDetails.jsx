fa48474890907a5e651ddb41c000455122c22dd7
omarfarhatul <omarfarhatulhossain@gmail.com>
2026-03-11 11:39:34 +0600

    single book show

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const BookDetails = () => {
+    const {id}=useParams();
+    const bookId=parseInt(id);
+    const data=useLoaderData();
+    const singleBook=data.find(book=>book.bookId===bookId);
+    const {bookName, image}=singleBook;
+
     return (
         <div>
-            <h1>Details</h1>
+            <img src={image} alt="" />
+            <h5>{bookName}</h5>
         </div>
     );
 };
